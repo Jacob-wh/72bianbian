f258da0eb834cc41070ef79deb4b727ce8b08872
Sun <1113862469@qq.com>
2019-04-06 10:42:22 +0800

    提交爬虫代码,改变主页和新闻模块中的跳转

--- information/info/modeules/index/views.py
+++ information/info/modeules/index/views.py
@@ -99,7 +99,7 @@ def index():
         "category_li": category_li,
         "cid": cid
     }
-    return render_template('news/index.html',
+    return render_template('supindex.html',
                            data=data,
                            )
 
@@ -112,13 +112,59 @@ def favicon():
     return current_app.send_static_file('news/favicon.ico')
 
 
-@index_blu.route('/cluster')
-def cluster():
-    print('hhhhhhhhhhhhh')
-    return jsonify({"hahha": "hahahh"})
+# @index_blu.route('/cluster')
+# def cluster():
+#     print('hhhhhhhhhhhhh')
+#     return jsonify({"hahha": "hahahh"})
 
 
-@index_blu.route('/supindex')
+# @index_blu.route('/')
+# def supindex():
+#     # data = "ccc"
+#     return render_template("supindex.html")
+@index_blu.route('/index')
+@user_login_data
 def supindex():
-    data = "ccc"
-    return render_template("/supindex.html", data=data)
+    # redis_store.set('name', 'huanghaibo')
+    """
+    显示首页
+    1,如果用户已经登录，将当前登录用户的数据传到模板中，显示
+    :return:
+    """
+    user_id = session.get('user_id', None)
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+            print(user)
+        except Exception as e:
+            current_app.logger.error(e)
+    user = g.user
+    #  右侧新闻排行逻辑
+    news_list = []
+    cid = request.args.get("cid", "1")
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(6)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_dict_li = []
+    # 遍历对象列表
+    for news in news_list:
+        # print(news)
+        news_dict_li.append(news.to_basic_dict())
+    # 查询分类数据， 通过末班形式展示出来
+    categories = Category.query.all()
+    category_li = []
+    for category in categories:
+        category_li.append(category)
+
+    data = {
+        "user": user.to_dict() if user else None,
+        "news_dict_li": news_dict_li,
+        "category_li": category_li,
+        "cid": cid
+    }
+    return render_template('news/index.html',
+                           data=data,
+                           )
